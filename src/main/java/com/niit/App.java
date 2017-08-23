63b419aee09fdc850aa5b783aa643207640eea40
Neeraj Khanna <neeraj.khanna@gmail.com>
2017-08-22 23:13:32 -0700

    done

--- src/main/java/com/niit/App.java
+++ src/main/java/com/niit/App.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
+@EnableTurbine
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
